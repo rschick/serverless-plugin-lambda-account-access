4fab84bc98001de6bb84c17a7e2e0006667e2c5e
AntonBazhal <am.bazhal@gmail.com>
2022-04-19 21:08:17 -0400

    Fix principals schema

--- add-permissions.js
+++ add-permissions.js
@@ -1,6 +1,23 @@
  'use strict';
 
-const STRING_OR_STRING_ARRAY = {
+const STRING_OR_NUMBER_SCHEMA = {
+  anyOf: [
+    { type: 'string' },
+    { type: 'integer' },
+  ],
+};
+
+const PRINCIPALS_SCHEMA = {
+  anyOf: [
+    {
+      type: 'array',
+      items: STRING_OR_NUMBER_SCHEMA
+    },
+    STRING_OR_NUMBER_SCHEMA
+  ]
+};
+
+const STRING_OR_STRING_ARRAY_SCHEMA = {
   anyOf: [
     {
       type: 'array',
@@ -18,7 +35,7 @@ const ROLE_SCHEMA = {
   type: 'object',
   properties: {
     name: { type: 'string' },
-    principals: STRING_OR_STRING_ARRAY,
+    principals: PRINCIPALS_SCHEMA,
     allowTagSession: { type: 'boolean' },
     maxSessionDuration: {
       type: 'integer',
@@ -52,7 +69,7 @@ const ACCESS_SCHEMA = {
             policy: {
               type: 'object',
               properties: {
-                principals: STRING_OR_STRING_ARRAY,
+                principals: PRINCIPALS_SCHEMA,
                 qualifier: { type: 'string' },
               },
               required: ['principals']
@@ -82,7 +99,7 @@ module.exports = class AwsAddLambdaAccountPermissions {
       if (serverless.configSchemaHandler.defineFunctionProperties) {
         serverless.configSchemaHandler.defineFunctionProperties('aws', {
           properties: {
-            allowAccess: STRING_OR_STRING_ARRAY,
+            allowAccess: STRING_OR_STRING_ARRAY_SCHEMA,
           },
         });
       }
